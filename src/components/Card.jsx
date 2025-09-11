2c84c4fc0e9558fc36af638bc5d3a5780fe0d530
Ahmed <ahmedelsharawy117@gmail.com>
2025-09-11 22:40:53 +0300

    chore: add id

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,36 @@
+import { Link } from "react-router-dom";
 import { format } from "../utils/formattedDate";
 
-function Card({ src, title, name, release_date, first_air_date }) {
+function Card({
+	id,
+	src,
+	title,
+	name,
+	release_date,
+	first_air_date,
+	media_type,
+}) {
 	return (
-		<div className="flex flex-col text-center bg-white rounded-2xl shadow-md hover:scale-105 transition-transform duration-200">
-			<div className="h-[250px] overflow-hidden w-full">
-				<img
-					src={src}
-					alt={title || name}
-					className="w-full h-full object-cover rounded-t-2xl"
-				/>
-			</div>
+		<Link to={`/${media_type}/${id}`}>
+			<div className="flex flex-col bg-white hover:scale-105 transition-transform duration-200">
+				<div className="h-[250px] overflow-hidden w-full">
+					<img
+						src={src}
+						alt={title || name}
+						className="w-full h-full object-cover rounded-md"
+					/>
+				</div>
 
-			<div className="p-4 flex flex-col text-center">
-				<p className="font-bold text-wrap text-gray-800 truncate w-[150px]">
-					{title || name}
-				</p>
-				<p className="text-sm text-gray-500">
-					{format(release_date || first_air_date)}
-				</p>
+				<div className="p-2 flex flex-col">
+					<p className="font-bold text-sm text-wrap text-gray-800 truncate w-[150px]">
+						{title || name}
+					</p>
+					<p className="text-[12px] text-gray-500">
+						{format(release_date || first_air_date)}
+					</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
 
