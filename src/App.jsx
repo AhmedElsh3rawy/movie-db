5bbe6ebe6f665a1c0fd7185320c2840fc3d6c628
Ahmed <ahmedelsharawy117@gmail.com>
2025-09-15 00:50:20 +0300

    feat: tv shows route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { WatchlistProvider } from "./context/WatchlistContext";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
+import TVShows from "./pages/TVShows";
 
 const router = createBrowserRouter([
 	{
 		element: <Layout />,
 		children: [
 			{ path: "/", element: <Home /> },
+			{ path: "/tv", element: <TVShows /> },
 			{ path: "/login", element: <Login /> },
 			{ path: "/register", element: <Register /> },
 			{ path: "*", element: <NotFound /> },
@@ -18,7 +21,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+	return (
+		<>
+			<WatchlistProvider>
+				<RouterProvider router={router} />;
+			</WatchlistProvider>
+		</>
+	);
 }
 
 export default App;
