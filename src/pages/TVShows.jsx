9f798f911daabd43b19288f6366476753a3ad500
Ahmed <ahmedelsharawy117@gmail.com>
2025-09-15 00:49:03 +0300

    feat: tv shows page

---
+++ src/pages/TVShows.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CardList from "../components/CardList";
+import { API_KEY, BASE_URL } from "../config";
+
+function TVShows() {
+	const [data, setData] = useState([]);
+	const [page, setPage] = useState(1);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		(async () => {
+			const url = `${BASE_URL}/tv/popular?page=${page}&api_key=${API_KEY}`;
+			try {
+				setLoading(true);
+				const res = await axios.get(url);
+
+				setData((prev) => {
+					const merged = [...prev, ...res.data.results];
+					return [...new Map(merged.map((item) => [item.id, item])).values()];
+				});
+			} catch (err) {
+				console.error("Error fetching data:", err);
+			} finally {
+				setLoading(false);
+			}
+		})();
+	}, [page]);
+
+	return (
+		<main className="flex flex-col items-center">
+			{!loading && (
+				<>
+					<CardList data={data} />
+
+					<button
+						className="bg-sky-500 hover:bg-sky-600 text-white font-bold rounded w-[200px] py-1 px-5 cursor-pointer mt-2"
+						type="button"
+						onClick={() => setPage((prev) => prev + 1)}
+					>
+						Load More
+					</button>
+				</>
+			)}
+		</main>
+	);
+}
+
+export default TVShows;
